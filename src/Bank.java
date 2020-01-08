cfb243cdce406dfd3e1b8ac5a52965afa8b3bda7
Huy1411 <nguyenxuanhuy1411@gmail.com>
2020-01-08 09:07:51 +0700

    Back and Flight

---
+++ src/Bank.java
@@ -0,0 +1,30 @@
+public class Bank {
+    private double balance;
+    private double rate;
+
+    public Bank(double balance, double rate) {
+        this.balance = balance;
+        this.rate = rate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double calculateInterest() {
+        double interest;
+        return interest = balance * (rate / 1200);
+    }
+}
